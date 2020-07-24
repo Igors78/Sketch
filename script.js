f143f0970f761d074a8f32b9b82634377add2eb3
Igors78 <oleinikovsigors@inbox.lv>
2020-07-25 00:53:57 +0300

    Finished for today

--- script.js
+++ script.js
@@ -31,29 +31,20 @@ button.addEventListener("click", function () {
     setContainer(squares, container);
 });
 function setContainer(squares, container) {
-    squares.forEach(sqr => sqr.style.backgroundColor = "rgb(0, 0, 0)"
-        );
+        container.innerHTML = '';
         let grid = prompt('Set the squares quantity in the grid','16');
         let sqrGrid = grid * grid;
-        let length = squares.length;
-        if (length < sqrGrid) {
-            for(let i = length; i < sqrGrid; i++) {
+        
+            for(let i = 0; i < sqrGrid; i++) {
                 squares[i] = document.createElement('div');
                 squares[i].style.cssText = "border: 1px solid grey; height: auto; padding: 0; margin: 0";
-                squares[i].style.backgroundColor = "rgb(0, 0, 0)";
                 squares[i].addEventListener("mouseenter", changeColor);
                 container.appendChild(squares[i]);
             }
-        }else if(length > sqrGrid) {
-            for(let i = sqrGrid; i < length; i++){
-                let child = container.lastElementChild;
-                container.removeChild(child);
-                squares.pop();
-            }
-        }
         let str = '';
-        for (let i = 0; i < Number(grid); i++){
-            str += "auto";
-        }
+            for (let i = 0; i < Number(grid); i++) {
+            str += "auto ";
+        
         container.style.cssText = `grid-template-columns: ${str};`;
         }
+    }
